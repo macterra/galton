d5dc76785cb4aef8756fdfb5c3c6130a0f71db0b
David McFadzean <davidmc@gmail.com>
2010-11-02 20:52:28 -0600

    Added test for sum of means

--- test.py
+++ test.py
@@ -15,16 +15,13 @@ class TestMonteCarlo(unittest.TestCase):
                 
     def simTest(self, type, getResult):
         for i in range(10):
-            x = random.randint(1,100)
+            x = random.random() * 100
             risk = random.choice(self.risks)
             results = galton.RunMonteCarlo(10000, [galton.Task(x, type, risk)])
             y = getResult(results)
             #print i, type, "=", x, y, risk
             self.assertCloseEnough(x, y)
             self.assertCloseEnough(results['risk'], galton.RiskMap[risk])
-            
-    def test_mean(self):
-        self.simTest('mean', lambda res: res['mean'])
         
     def test_mode(self):
         self.simTest('mode', lambda res: res['mode'])
@@ -44,6 +41,17 @@ class TestMonteCarlo(unittest.TestCase):
     def test_p90(self):
         self.simTest('p90', lambda res: res['cumprob'][90][0])
                
+    def test_mean1(self):
+        self.simTest('mean', lambda res: res['mean'])
+        
+    def test_mean2(self):
+        for i in range(10):
+            estimates = [random.random() * 100 for t in range(20)]
+            tasks = [galton.Task(e, 'mean', random.choice(self.risks)) for e in estimates]
+            results = galton.RunMonteCarlo(10000, tasks)
+            #print "meanTest", sum(estimates), results['mean']
+            self.assertCloseEnough(sum(estimates), results['mean'])
+            
 if __name__ == '__main__':
     unittest.main()
 
