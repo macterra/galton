608f4c939a27b48bfdb2e9937c1356aad7f7df6b
David McFadzean <davidmc@gmail.com>
2010-11-02 20:37:41 -0600

    Consolidated all tests into one main test

--- test.py
+++ test.py
@@ -6,53 +6,44 @@ import math
 class TestMonteCarlo(unittest.TestCase):
 
     def setUp(self):
-        self.risks = ['none', 'low', 'medium', 'high', 'very high']
-        self.types = ['mode', 'mean', 'p50', 'p60', 'p70', 'p80', 'p90']
+        self.risks = galton.RiskMap.keys()
         
     def assertCloseEnough(self, a, b):
         x = abs(math.log10(a/b))
+        #print a, b, x
         self.assert_(x < 0.02, "x = %d" % (x))
-        
-    def confidenceTest(self, type, confidence):
+                
+    def simTest(self, type, getResult):
         for i in range(10):
             x = random.randint(1,100)
             risk = random.choice(self.risks)
             results = galton.RunMonteCarlo(10000, [galton.Task(x, type, risk)])
-            p = results['cumprob'][confidence][0]
-            #print i, type, "=", x, p, risk
-            self.assertCloseEnough(x, p)
-            self.assertCloseEnough(results['risk'], galton.RiskMap[risk])
-            
-    def test_p50(self):
-        self.confidenceTest('p50', 50)
-        
-    def test_p60(self):
-        self.confidenceTest('p60', 60)
-        
-    def test_p70(self):
-        self.confidenceTest('p70', 70)
-        
-    def test_p80(self):
-        self.confidenceTest('p80', 80)
-        
-    def test_p90(self):
-        self.confidenceTest('p90', 90)
-        
-    def averageTest(self, type):
-        for i in range(10):
-            x = random.randint(1,100)
-            risk = random.choice(self.risks)
-            results = galton.RunMonteCarlo(10000, [galton.Task(x, type, risk)])
-            avg = results[type]
-            self.assertCloseEnough(x, avg)
+            y = getResult(results)
+            #print i, type, "=", x, y, risk
+            self.assertCloseEnough(x, y)
             self.assertCloseEnough(results['risk'], galton.RiskMap[risk])
             
     def test_mean(self):
-        self.averageTest('mean')
+        self.simTest('mean', lambda res: res['mean'])
         
     def test_mode(self):
-        self.averageTest('mode')
-            
+        self.simTest('mode', lambda res: res['mode'])
+        
+    def test_median(self):
+        self.simTest('p50', lambda res: res['cumprob'][50][0])
+        
+    def test_p60(self):
+        self.simTest('p60', lambda res: res['cumprob'][60][0])
+        
+    def test_p70(self):
+        self.simTest('p70', lambda res: res['cumprob'][70][0])
+        
+    def test_p80(self):
+        self.simTest('p80', lambda res: res['cumprob'][80][0])
+        
+    def test_p90(self):
+        self.simTest('p90', lambda res: res['cumprob'][90][0])
+               
 if __name__ == '__main__':
     unittest.main()
 
